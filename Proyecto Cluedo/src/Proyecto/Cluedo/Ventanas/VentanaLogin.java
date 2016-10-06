749728043fe5ac479ab3f98ad9ddc0cf13537ebb
Ander Arguiñano <ander.arguinano@opendeusto.es>
2016-10-06 18:35:48 +0200

    PLanificacion Inicial del Proyecto

---
+++ Proyecto Cluedo/src/Proyecto/Cluedo/Ventanas/VentanaLogin.java
@@ -0,0 +1,20 @@
+package Proyecto.Cluedo.Ventanas;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class VentanaLogin extends JFrame {
+
+	public static void main (String [] args){
+		
+		
+	}
+	
+	public VentanaLogin(){
+		
+		
+		
+	}
+	
+	
+}
