052e7aec1c30cb089557cca25ae5f18bb61549cd
Ander Arguiñano <ander.arguinano@opendeusto.es>
2016-10-13 16:12:33 +0200

    Creada la ventana para esperar a la conexion

---
+++ Proyecto Cluedo/src/Proyecto/Cluedo/Ventanas/VentanaConectando.java
@@ -0,0 +1,72 @@
+package Proyecto.Cluedo.Ventanas;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class VentanaConectando extends JFrame {
+
+	/**
+	 * Label que contiene la imagen de deusto
+	 */
+	private JLabel etiqueta = new JLabel();
+
+	/**
+	 * Imagen de deusto
+	 */
+
+	private ImageIcon imageIcon;
+
+	public static void main(String[] args) {
+		VentanaConectando ventana = new VentanaConectando();
+		ventana.setVisible(true);
+	}
+
+	public VentanaConectando() {
+
+		// Inicializamos el JFrame
+
+		setSize(new Dimension(700, 500));
+		setUndecorated(true);
+		setLocationRelativeTo(null);
+		setResizable(false);
+
+		// Generamos los componentes
+
+		imageIcon = new ImageIcon();
+
+		/**
+		 * Label que contiene el texto Conectando
+		 */
+
+		JLabel conectando = new JLabel("Conectando...");
+
+		// Establecemos el formato
+
+		getContentPane().setLayout(null);
+		getContentPane().add(etiqueta);
+		getContentPane().add(conectando);
+
+		conectando.setBounds(210, 130, 500, 500);
+
+		conectando.setForeground(Color.BLUE);
+
+		conectando.setFont(new Font("Tahoma", Font.BOLD, 40));
+
+		etiqueta.setBounds(50, 20, 600, 400);
+
+		try {
+			imageIcon = new ImageIcon(VentanaConectando.class.getResource("Imagenes/Gif Deusto.gif").toURI().toURL());
+		} catch (Exception e) {
+			System.out.println("No se ha encontrado el archivo");
+		}
+
+		etiqueta.setIcon(imageIcon);
+
+	}
+
+}
