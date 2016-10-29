532a1b45f3b5d150f25e907b47afe8bcbfecda25
Ander Arguiñano <ander.arguinano@opendeusto.es>
2016-10-29 15:41:20 +0200

    Mejorada la ventana de las notas

--- Proyecto Cluedo/src/Proyecto/Cluedo/Ventanas/ventana.java
+++ Proyecto Cluedo/src/Proyecto/Cluedo/Ventanas/ventana.java
@@ -1,21 +1,21 @@
 package Proyecto.Cluedo.Ventanas;
 
-import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics2D;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -31,26 +31,55 @@ import Proyecto.Cluedo.Ventanas.panelrosa;
 
 public class ventana extends JFrame{
 	
+	private Point coordenada;
+	
+	private class cuadrado extends JPanel {  
+
+		public cuadrado(){
+			setLayout(new BorderLayout());
+			add(new JLabel("."), BorderLayout.CENTER);
+			setLocation(coordenada);
+			setSize(5,5);
+			setBackground(Color.white);
+		}
+		
+	}
+	
 	private JPanel pventana=new JPanel();
+	
 	private panelrosa pdibujar;
+	
 	private JPanel ppizarra=new JPanel();
+	
 	private JPanel ptabla=new JPanel();
+	
 	private JTable tabla=new JTable(new MyTableModel() );
+	
 	private JTable tabarmas=new JTable(new MyTableModelArmas());
+	
 	private JTable tablugares=new JTable(new MyTableModelLugares());
+	
 	private JPanel ptablugares=new JPanel();
+	
 	private JSplitPane spventana;	
+	
 	private JPanel ptabarmas=new JPanel();
+	
 	private JLabel ltitulo=new JLabel();
+	
 	private panelrosa phojap;
+	
 	private JScrollPane scrollphojap;
+	
 	private JPanel phoja=new JPanel();
+	
 	private static int[][] mibaraja=new int[3][4];
 
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
-		mibaraja[0][0]=0;
+		
+		mibaraja[0][0]=5;
 		mibaraja[0][1]=1;
 		mibaraja[0][2]=2;
 		mibaraja[0][3]=3;
@@ -62,14 +91,22 @@ public class ventana extends JFrame{
 		mibaraja[2][1]=4;
 		mibaraja[2][2]=5;
 		mibaraja[2][3]=0;
+		
 		JFrame f=new ventana();
+		
 		f.setVisible(true);
 	}
+	
 	public ventana(){
+		
 		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
-		setSize( 640, 480 );
-		setMinimumSize(new Dimension(750,300));
+		
+		setSize( 1330, 730 );
+		
+		setMinimumSize(new Dimension(900,500));
+		
 		setResizable( true );
+		
 		//pventana es el mayor panel de la ventana dividida en dos partes
 		//getContentPane().add(pventana);
 		//pventana.setLayout( new BorderLayout() );
@@ -77,11 +114,15 @@ public class ventana extends JFrame{
 		//pdibujar.setSize(900,720);
 		//pdibujar panel principal de la parte izquierda
 		//ppizarra.add(pdibujar);
+		
 		ppizarra.setBackground(Color.white);
+		
 		//fondo ppizarra
+		
 		ImageIcon image;		
-		image = new ImageIcon(ventana.class.getResource("Imagenes/pizarra.png"));
+		image = new ImageIcon(ventana.class.getResource("Imagenes/pizarra.jpg"));
 		pdibujar=new panelrosa(image.getImage());
+
 //		ImageIcon image;		
 //		image = new ImageIcon(VentanaApuntes.class.getResource("Imagenes/pizarra.png"));
 //		JLabel lpizarra=new JLabel();
@@ -91,7 +132,8 @@ public class ventana extends JFrame{
 //        pdibujar.add(lpizarra);
         //pventana.add(ppizarra,BorderLayout.WEST);
         //tabla armas
-        ptabarmas.setLayout(new BorderLayout());
+  
+		ptabarmas.setLayout(new BorderLayout());
 		ptabarmas.add(tabarmas.getTableHeader(),BorderLayout.NORTH);		
 		tabarmas.getColumn("Notas").setPreferredWidth(300);
 		tabarmas.getColumn("Sospechosos").setPreferredWidth(100);
@@ -130,7 +172,9 @@ public class ventana extends JFrame{
 		rendererlug.setOpaque(false);
 		tablugares.setDefaultRenderer(Object.class, rendererlug);
 		DefaultTableCellRenderer renderertit =new DefaultTableCellRenderer();
-		renderertit.setBackground(Color.CYAN);
+		renderertit.setBackground(Color.DARK_GRAY);
+		renderertit.setForeground(Color.white);
+		renderertit.setFont(new Font("System",Font.BOLD,12));
 		tablugares.getTableHeader().setDefaultRenderer(renderertit);
 		tabla.getTableHeader().setDefaultRenderer(renderertit);
 		ptablugares.add(tablugares,BorderLayout.CENTER);
@@ -138,12 +182,18 @@ public class ventana extends JFrame{
 		
 		//panel de fondo de la tabla
 		  ImageIcon imagehoja;			
-		  imagehoja = new ImageIcon(ventana.class.getResource("Imagenes/POSIT.jpg"));
+		  
+		  imagehoja = new ImageIcon(ventana.class.getResource("Imagenes/background.png"));
+		  
 		 // phoja=new panelrosa(imagehoja.getImage().getScaledInstance(750,1000 , Image.SCALE_DEFAULT));
+		 
 		  phojap=new panelrosa(imagehoja.getImage());
+		  
+		  
 		  
 			 
 		  // phoja.setLayout(new BoxLayout(phoja,BoxLayout.X_AXIS));
+		  
 		  phoja.setLayout(new BoxLayout(phoja,BoxLayout.Y_AXIS));
 		  ptabla.setOpaque(false);
 		  ptablugares.setOpaque(false);
@@ -152,16 +202,26 @@ public class ventana extends JFrame{
 		  rendererarmas.setOpaque(false);
 		  tabarmas.setDefaultRenderer(Object.class, rendererarmas);
 		  tabarmas.getTableHeader().setDefaultRenderer(renderertit);
+		   
+		  JLabel hueco=new JLabel("<html><body> <br> <br> <br> <br> <br></body></html>");
+		  
 		  phoja.add(ptabla);
 		  phoja.add(ptabarmas);
 		  phoja.add(ptablugares);
+		  phoja.add(hueco);
 		  phoja.setOpaque(false);
 		  phojap.setLayout(new BoxLayout(phojap,BoxLayout.X_AXIS));
 		  phojap.add(phoja);
+		  
+		 
+		  
+		  
+		  
 		  //pventana.add(phoja,BorderLayout.CENTER);
+		  
 		  //titulo
 		  ImageIcon imagetit = new ImageIcon(ventana.class.getResource("Imagenes/TITMISNOTAS.png"));
-		  ltitulo.setSize(200,200);
+		  ltitulo.setSize(200,150);
 	      Icon iconotit = new ImageIcon(imagetit.getImage().getScaledInstance(ltitulo.getWidth(), ltitulo.getHeight(), Image.SCALE_DEFAULT));
 	      ltitulo.setIcon(iconotit);
 	      //añadir boligrafo
@@ -175,7 +235,8 @@ public class ventana extends JFrame{
 	      Icon iconogarabato = new ImageIcon(imagegarabato.getImage().getScaledInstance(lgarabato.getWidth(), lgarabato.getHeight(), Image.SCALE_DEFAULT));
 	      lgarabato.setIcon(iconogarabato);
 	      phojap.add(lgarabato,0);
-	      lboli.setIcon(iconoboli);	      
+	      lboli.setIcon(iconoboli);	
+	      
 	      phoja.add(ltitulo,0);	  
 	      phojap.add(phoja);
 	      phojap.add(lboli);
@@ -186,19 +247,34 @@ public class ventana extends JFrame{
 	      spventana=new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,scrollphojap,pdibujar);
 	      getContentPane().add(spventana);			
         //escuchador del panel para dibujar
+	   
+	    pdibujar.setLayout(null);
+	    
+	  
+	    
         pdibujar.addMouseMotionListener( new MouseMotionListener() {
-			private Point pAnt = null;
+			//private Point pAnt = null;
 			@Override
 			public void mouseDragged(MouseEvent e) {
-				pAnt = e.getPoint();
+				/*pAnt = e.getPoint();
 				
 				Graphics2D g2 = (Graphics2D) pdibujar.getGraphics();
 				g2.setColor( Color.white );
 				g2.setStroke( new BasicStroke( 2f ) );
 				if (pAnt!=null) {
 					g2.drawLine( pAnt.x, pAnt.y, e.getX(), e.getY() );  // Dibuja líneas al arrastrar
+				*/
 				
+				coordenada = e.getPoint(); // Obtenemos la coordenada
+				if (coordenada.getX()<40||coordenada.getX()>pdibujar.getWidth()-40){
+					
+				}else if (coordenada.getY()<40||coordenada.getY()>pdibujar.getHeight()-40){
+					
 				}
+			else{
+				pdibujar.add(new cuadrado()); // AÃ±adimos el cuadrado
+				pdibujar.validate(); // Validamos
+			}
 				
 			}
 			@Override
@@ -207,9 +283,13 @@ public class ventana extends JFrame{
 				
 			}
 		});
+        
+
+        
+       
 	}
         
-        //Poner canddado y bloquear 
+        //Poner candado y bloquear 
         public  void ponerCandado(int[][]cartas,JTable tabla,int tipo){
         	for (int i=0;i<tabla.getRowCount();i++){
         	if(tipo==0){
@@ -224,11 +304,18 @@ public class ventana extends JFrame{
         	if(tipo==1){
             	if (((MyTableModelLugares) tabla.getModel()).isCellEditable(i, 2)==false){
             		//
-            		ImageIcon image= new ImageIcon(ventana.class.getResource("Imagenes/Candado.png"));
+            		
+            		ImageIcon imagen=new ImageIcon();
             		JLabel lpizarra=new JLabel();
-                    lpizarra.setSize(10,10);
-                    lpizarra.setOpaque(false);                    
-                    Icon icono = new ImageIcon(image.getImage().getScaledInstance(lpizarra.getWidth(), lpizarra.getHeight(), Image.SCALE_DEFAULT));
+                    lpizarra.setSize(10,10);      
+					try {
+						imagen = new ImageIcon(ventana.class.getResource("Imagenes/Candado.png").toURI().toURL());
+					} catch (MalformedURLException | URISyntaxException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+            		             
+                    Icon icono = new ImageIcon(imagen.getImage().getScaledInstance(lpizarra.getWidth(), lpizarra.getHeight(), Image.SCALE_DEFAULT));
                     tabla.setValueAt(icono,i,0);
             	}
         		
@@ -249,8 +336,8 @@ public class ventana extends JFrame{
 	    private String[] columnNames = {"Mio","Sospechosos",
 	                                    "     ","Notas"};
 	    private Object[][] data = {
-	    		{"",new String ("Inspector gadgets"),new Boolean(false),""},{"",new String("Paris Hilton"),new Boolean(false),""},{"",new String("Usain Bolt"),new Boolean(false),""},{"",new String("Socrates"),new Boolean(false),""},{"",new String("Amuriza"),new Boolean(false),""},{"","Minerva McGonagall",new Boolean(false),""},
-	         	{"","La Momia",new Boolean(false),""},{"","Jueza Alaya",new Boolean(false),""}};
+	    		{new ImageIcon (),new String ("Inspector gadgets"),new Boolean(false),""},{new ImageIcon (),new String("Paris Hilton"),new Boolean(false),""},{new ImageIcon (),new String("Usain Bolt"),new Boolean(false),""},{new ImageIcon (),new String("Socrates"),new Boolean(false),""},{new ImageIcon (),new String("Amuriza"),new Boolean(false),""},{new ImageIcon (),"Minerva McGonagall",new Boolean(false),""},
+	         	{new ImageIcon (),"La Momia",new Boolean(false),""},{new ImageIcon (),"Jueza Alaya",new Boolean(false),""}};
 
 	    public int getColumnCount() {
 	        return columnNames.length;
@@ -319,7 +406,7 @@ public class ventana extends JFrame{
 	    private String[] columnNames = {"Mio","Sospechosos",
 	                                    "     ","Notas"};
 	    private Object[][] data = {
-	    		{"",new String ("Pistola"),new Boolean(false),""},{"",new String("Biblia"),new Boolean(false),""},{"",new String("Bocata envenenado"),new Boolean(false),""},{"",new String("Chip"),new Boolean(false),""},{"",new String("Botella"),new Boolean(false),""},{"","Sarten",new Boolean(false),""},
+	    		{new ImageIcon (),new String ("Pistola"),new Boolean(false),""},{new ImageIcon (),new String("Biblia"),new Boolean(false),""},{new ImageIcon (),new String("Bocata envenenado"),new Boolean(false),""},{new ImageIcon (),new String("Chip"),new Boolean(false),""},{new ImageIcon (),new String("Botella"),new Boolean(false),""},{new ImageIcon (),"Sarten",new Boolean(false),""},
 	   };
 
 	    public int getColumnCount() {
@@ -386,8 +473,8 @@ public class ventana extends JFrame{
 	    private String[] columnNames = {"Mio","Sospechosos",
 	                                    "     ","Notas"};
 	    private Object[][] data = {
-	    		{"",new String ("Facultad ingeniería"),new Boolean(false),""},{"",new String("La Comercial"),new Boolean(false),""},{"",new String("La L"),new Boolean(false),""},{"",new String("La capilla"),new Boolean(false),""},{"",new String("Edificio Centenario"),new Boolean(false),""},{"","Edificio de letras",new Boolean(false),""},
-	    		{"","Biblioteca",new Boolean(false),""},{"","Zubiarte",new Boolean(false),""}};
+	    		{new ImageIcon(),new String ("Facultad ingeniería"),new Boolean(false),""},{new ImageIcon (),new String("La Comercial"),new Boolean(false),""},{new ImageIcon (),new String("La L"),new Boolean(false),""},{"",new String("La capilla"),new Boolean(false),""},{new ImageIcon (),new String("Edificio Centenario"),new Boolean(false),""},{new ImageIcon (),"Edificio de letras",new Boolean(false),""},
+	    		{new ImageIcon (),"Biblioteca",new Boolean(false),""},{new ImageIcon (),"Zubiarte",new Boolean(false),""}};
 
 	    public int getColumnCount() {
 	        return columnNames.length;
