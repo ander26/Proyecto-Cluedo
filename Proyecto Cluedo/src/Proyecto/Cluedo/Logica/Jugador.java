2c8c807813f5a9d86632e6efa7bc5a6ea643bb47
maideribarra <maideribarra@opendeusto.es>
2016-11-20 20:36:19 +0100

    repartir cartas de forma recursiva

--- Proyecto Cluedo/src/Proyecto/Cluedo/Logica/Jugador.java
+++ Proyecto Cluedo/src/Proyecto/Cluedo/Logica/Jugador.java
@@ -1,67 +1,49 @@
 package Proyecto.Cluedo.Logica;
 
-import java.awt.image.BufferedImage;
-import java.sql.Connection;
 import java.util.ArrayList;
 
 public class Jugador {
-	
-	private GestionBaseDeDatos gestion = new GestionBaseDeDatos();
-	
-	private ArrayList<ArrayList<Integer>> MisCartas;
-	
-	private String Ficha;
-	
+	private ArrayList<ArrayList<String>> MisCartas;
+	private String Monigote;
 	private int Lugar;
-	
 	private double PosicionMuñeco;
-	
 	private int Codigo;
-	
 	private int Turno;
-	
 	private String Usuario;
-	
 	private int CodigoPartida;
 
-	private BufferedImage dibujo;
 	
 
 	public Jugador() {
-	
 		
-	}
-	
-	public Jugador (String Ficha, String Usuario, int CodigoPartida,Connection conexion){
+		MisCartas = new ArrayList();
+		ArrayList<String> sospechosos=new ArrayList();
+		ArrayList<String> lugar=new ArrayList();
+		ArrayList<String> arma=new ArrayList();
+		ArrayList<String> comodin=new ArrayList();
+		MisCartas.add(sospechosos);
+		MisCartas.add(arma);
+		MisCartas.add(lugar);
+		MisCartas.add(comodin);
 		
-		this.Ficha=Ficha;
-		this.Codigo=generadorCodigo(gestion.obtenerCodigoJugador(conexion));
-		this.Usuario=Usuario;
-		this.CodigoPartida=CodigoPartida;
-		MisCartas= new ArrayList<>();
+		this.Monigote = null;
 		this.Turno=0;
-		
 		this.Lugar=-1;
 		this.PosicionMuñeco=-1;
-		
-		dibujo=null;
-		
+		this.Codigo=-1;
+		this.Usuario="";
+		this.CodigoPartida=-1;
 		
 	}
-	
-	
 	public int getCodigoPartida() {
 		return CodigoPartida;
 	}
-	
 	public void setCodigoPartida(int codigoPartida) {
 		CodigoPartida = codigoPartida;
 	}
-	
 	public String getUsuario() {
 		return Usuario;
 	}
-	
 	public void setUsuario(String usuario) {
 		Usuario = usuario;
 	}
@@ -76,69 +58,38 @@ public class Jugador {
 		Lugar = lugar;
 	}
 
-	public String getFicha() {
-		return Ficha;
+	public String getMonigote() {
+		return Monigote;
 	}
 
-	public void setMonigote(String Ficha) {
-		this.Ficha = Ficha;
+	public void setMonigote(String monigote) {
+		Monigote = monigote;
 	}
 
 	
 	public double getPosicionMuñeco() {
 		return PosicionMuñeco;
 	}
-	
 	public void setPosicionMuñeco(double posicionMuñeco) {
 		PosicionMuñeco = posicionMuñeco;
 	}
-	
 	public int getCodigo() {
 		return Codigo;
 	}
-	
 	public void setCodigo(int codigo) {
 		Codigo = codigo;
 	}
-	
 	public int getTurno() {
 		return Turno;
 	}
-	
-	public ArrayList<ArrayList<Integer>> getMisCartas() {
+	public ArrayList<ArrayList<String>> getMisCartas() {
 		return MisCartas;
 	}
 
-	public void setMisCartas(ArrayList<ArrayList<Integer>> misCartas) {
+	public void setMisCartas(ArrayList<ArrayList<String>> misCartas) {
 		MisCartas = misCartas;
 	}
-
-	public void setCarta(int indice,int num) {
-		MisCartas.get(indice).add(num);
-	}
-
-	public BufferedImage getDibujo() {
-		return dibujo;
-	}
-
-	public void setDibujo(BufferedImage dibujo) {
-		this.dibujo = dibujo;
-	}
-	
-	public int generadorCodigo (ArrayList <Integer> listaCodigos ){
-		
-		int contador =1;
-	
-		
-		for (Integer i: listaCodigos){
-			if (contador==i){
-				contador++;
-				
-			}else{
-				return contador;
-			}
-		}
-		
-		return contador;
+	public void setCarta(int indice,String nombre) {
+		MisCartas.get(indice).add(nombre);
 	}
 }
--- Proyecto Cluedo/src/Proyecto/Cluedo/Logica/Propiedades.java
+++ Proyecto Cluedo/src/Proyecto/Cluedo/Logica/Propiedades.java
@@ -64,10 +64,159 @@ public class Propiedades {
 		this.baraja.add(alcomodines);
 	
 	}
-	//Escoje las 3 csrtas del asesinato y reparte las demas entre los jugadores
-	public  void repartirCartas(Jugador [] arJugadores){
+//	//Escoje las 3 csrtas del asesinato y reparte las demas entre los jugadores
+//	public  void repartirCartas(Jugador [] arJugadores){
+//		
+//		//Seleccion de las crtas que hacen el asesinato(las que en la vida real se meten en un sobre)
+//		int [] contcartas=new int[arJugadores.length];
+//		for(int i=0;i<3;i++){
+//			int num=(int) (Math.random()*10);
+//			while(num>=baraja.get(i).size()){
+//				num=(int) (Math.random()*10);
+//			}
+//			baraja.get(i).get(num).setCulpable(true);
+//			baraja.get(i).get(num).setSeleccionada(true);
+//			;
+//		}
+//		
+//	for(int j=0;j<3;j++){
+//		for(int g=0;g<arJugadores.length;g++){
+//			ArrayList<Integer> alcartaspalo= new ArrayList();
+//			for(int h=0;h<(int)((baraja.get(j).size()-1)/arJugadores.length);h++){	
+//				
+//				boolean buscarmas=true;
+//				int num;
+//				while(buscarmas){
+//					num=(int) (Math.random()*10);
+//					if(num<baraja.get(j).size() && baraja.get(j).get(num).isSeleccionada()==false){
+//						baraja.get(j).get(num).setSeleccionada(true);
+//						alcartaspalo.add(num);
+//						System.out.println(num+" "+h);
+//						contcartas[g]=contcartas[g]+1;
+//						buscarmas=false;
+//						
+//						
+//					}
+//					
+//				}
+//				
+//				
+//			}
+//			arJugadores[g].getMisCartas().add(alcartaspalo);
+//			
+//			
+//		}
+//		
+//		if((baraja.get(j).size()-1)%numJugadores!=0){
+//			System.out.println("entro");
+//			//int numcarpepartir=baraja.get(j).size()%numJugadores;
+//			for(Cartas it:baraja.get(j)){
+//				
+//				if(it.isSeleccionada()==false){
+//					for(int l=1;l<arJugadores.length;l++){
+//						if(l==0){
+//							if(contcartas[l]<contcartas[l-1]){
+//								int numindice=baraja.get(j).indexOf(it);
+//								arJugadores[l].setCarta(j,numindice);
+//								contcartas[l]=contcartas[l]+1;	
+//								baraja.get(j).get(numindice).setSeleccionada(true);
+//								l=arJugadores.length;
+//								
+//							}
+//						}
+//						if(l==arJugadores.length-1){
+//							if(contcartas[l]<contcartas[l-1]){
+//								int numindice=baraja.get(j).indexOf(it);
+//								arJugadores[l].setCarta(j,numindice);
+//								contcartas[l]=contcartas[l]+1;	
+//								baraja.get(j).get(numindice).setSeleccionada(true);
+//							}
+//							else{
+//								int numindice=baraja.get(j).indexOf(it);
+//								arJugadores[0].setCarta(j,numindice);
+//								contcartas[0]=contcartas[0]+1;
+//								baraja.get(j).get(numindice).setSeleccionada(true);
+//							}
+//							
+//						}
+//						else{
+//							if(contcartas[l]<contcartas[l-1]){
+//								int numindice=baraja.get(j).indexOf(it);
+//								arJugadores[l].setCarta(j,numindice);
+//								contcartas[l]=contcartas[l]+1;								
+//								l=arJugadores.length;
+//								baraja.get(j).get(numindice).setSeleccionada(true);
+//							}
+//						}
+//					
+//				}
+//				}
+//				}
+//		
+//		
+//			
+//		}
+//	}
+//	for(int h=0;h<contcartas.length;h++){
+//		ArrayList<Integer> alcartaspalo= new ArrayList();
+//		arJugadores[h].getMisCartas().add(alcartaspalo);
+//		int numero=0;
+//		if(((numTotCartas-3)%numJugadores)!=0){
+//			numero=1;
+//		}
+//		if(contcartas[h]<(int)((numTotCartas-3)/numJugadores)+numero){
+//			System.out.println((numTotCartas-3)/numJugadores);
+//			System.out.println(numTotCartas-3);
+//			System.out.println(numJugadores);
+//			//System.out.println(contcartas[h]);
+//			arJugadores[h].setCarta(3,0);				
+//			contcartas[h]=contcartas[h]+1;
+//			
+//			
+//			
+//		}
+//	}
+//	
+//	}
+	
+	public void RepartirCartas(Jugador [] arJugadores){
+		int k=0;
+		ArrayList<Cartas> al=new ArrayList();
+		barajear(0);
+		barajear(1);
+		barajear(2);
+		al.addAll(baraja.get(0));
+		al.addAll(baraja.get(1));
+		al.addAll(baraja.get(2));
+		for(int i=0;i<al.size();i++){
+			System.out.println(al.get(i).getNombre());
+		}
+		while((al.size()-3)%numJugadores!=0){
+			al.add(baraja.get(3).get(k));
+			k=k+1;
+			
+		}
+		for(int i=0;i<al.size();i++){
+			System.out.println(al.get(i).getNombre());
+		}
+		ArrayList<Cartas> alcartasParaRepartir=meterAlSobre(arJugadores,al);
+		RepartirRecursivo(0,0,arJugadores,alcartasParaRepartir);
 		
-		//Seleccion de las crtas que hacen el asesinato(las que en la vida real se meten en un sobre)
+	}
+	public void RepartirRecursivo(int jug,int baraja,Jugador []arJug,ArrayList<Cartas> acartas){
+		if(baraja==acartas.size()){
+			
+		}else{
+			if(jug==numJugadores){
+				jug=0;
+			}
+			arJug[jug].setCarta(TipoCartaToInt(acartas.get(baraja).getTipo()),acartas.get(baraja).getNombre() );
+			RepartirRecursivo(jug+1,baraja+1, arJug, acartas);
+		}
+		
+		
+	}
+	public ArrayList<Cartas> meterAlSobre(Jugador []arJugadores,ArrayList<Cartas> acartas){
 		int [] contcartas=new int[arJugadores.length];
 		for(int i=0;i<3;i++){
 			int num=(int) (Math.random()*10);
@@ -75,108 +224,43 @@ public class Propiedades {
 				num=(int) (Math.random()*10);
 			}
 			baraja.get(i).get(num).setCulpable(true);
-			baraja.get(i).get(num).setSeleccionada(true);
-			;
+			acartas.remove(baraja.get(i).get(num));
 		}
+		return acartas;
 		
-	for(int j=0;j<3;j++){
-		for(int g=0;g<arJugadores.length;g++){
-			ArrayList<Integer> alcartaspalo= new ArrayList();
-			for(int h=0;h<(int)((baraja.get(j).size()-1)/arJugadores.length);h++){	
-				
-				boolean buscarmas=true;
-				int num;
-				while(buscarmas){
-					num=(int) (Math.random()*10);
-					if(num<baraja.get(j).size() && baraja.get(j).get(num).isSeleccionada()==false){
-						baraja.get(j).get(num).setSeleccionada(true);
-						alcartaspalo.add(num);
-						System.out.println(num+" "+h);
-						contcartas[g]=contcartas[g]+1;
-						buscarmas=false;
-						
-						
-					}
-					
-				}
-				
-				
+	}
+	public int TipoCartaToInt(TipoCarta tc){
+		if(tc==TipoCarta.SOSPECHOSO){
+			return 0;
+		}else if(tc==TipoCarta.ARMA){
+			return 1;
+		}else if(tc==TipoCarta.LUGAR){
+			return 2;
+		}else{
+			return 3;
+		}
+	}
+	public void barajear(int i){
+		for (int j=0;j<300;j++){
+			int num=(int) (Math.random()*10);
+			while(num>=baraja.get(i).size()){
+				num=(int) (Math.random()*10);
 			}
-			arJugadores[g].getMisCartas().add(alcartaspalo);
-			
+			int numdos=(int) (Math.random()*10);
+			while(numdos>=baraja.get(i).size()){
+				numdos=(int) (Math.random()*10);
+			}
+			Cartas origen=baraja.get(i).get(num);
+			Cartas destino=baraja.get(i).get(numdos);
+//			baraja.get(i).add(numdos,origen);
+//			baraja.get(i).add(num,destino);
+//			baraja.get(i).remove(num+1);
+//			baraja.get(i).remove(numdos+1);
+			baraja.get(i).set(numdos, origen);
+			baraja.get(i).set(num,destino);
 			
 		}
 		
-		if((baraja.get(j).size()-1)%numJugadores!=0){
-			System.out.println("entro");
-			//int numcarpepartir=baraja.get(j).size()%numJugadores;
-			for(Cartas it:baraja.get(j)){
-				
-				if(it.isSeleccionada()==false){
-					for(int l=1;l<arJugadores.length;l++){
-						if(l==0){
-							if(contcartas[l]<contcartas[l-1]){
-								int numindice=baraja.get(j).indexOf(it);
-								arJugadores[l].setCarta(j,numindice);
-								contcartas[l]=contcartas[l]+1;	
-								baraja.get(j).get(numindice).setSeleccionada(true);
-								l=arJugadores.length;
-								
-							}
-						}
-						if(l==arJugadores.length-1){
-							if(contcartas[l]<contcartas[l-1]){
-								int numindice=baraja.get(j).indexOf(it);
-								arJugadores[l].setCarta(j,numindice);
-								contcartas[l]=contcartas[l]+1;	
-								baraja.get(j).get(numindice).setSeleccionada(true);
-							}
-							else{
-								int numindice=baraja.get(j).indexOf(it);
-								arJugadores[0].setCarta(j,numindice);
-								contcartas[0]=contcartas[0]+1;
-								baraja.get(j).get(numindice).setSeleccionada(true);
-							}
-							
-						}
-						else{
-							if(contcartas[l]<contcartas[l-1]){
-								int numindice=baraja.get(j).indexOf(it);
-								arJugadores[l].setCarta(j,numindice);
-								contcartas[l]=contcartas[l]+1;								
-								l=arJugadores.length;
-								baraja.get(j).get(numindice).setSeleccionada(true);
-							}
-						}
-					
-				}
-				}
-				}
-		
-		
-			
-		}
-	}
-	for(int h=0;h<contcartas.length;h++){
-		ArrayList<Integer> alcartaspalo= new ArrayList();
-		arJugadores[h].getMisCartas().add(alcartaspalo);
-		int numero=0;
-		if(((numTotCartas-3)%numJugadores)!=0){
-			numero=1;
-		}
-		if(contcartas[h]<(int)((numTotCartas-3)/numJugadores)+numero){
-			System.out.println((numTotCartas-3)/numJugadores);
-			System.out.println(numTotCartas-3);
-			System.out.println(numJugadores);
-			//System.out.println(contcartas[h]);
-			arJugadores[h].setCarta(3,0);				
-			contcartas[h]=contcartas[h]+1;
-			
-			
-			
-		}
-	}
-	
 	}
 
 	public int getNumTotArmas() {
@@ -237,22 +321,22 @@ public class Propiedades {
 	public static void main (String [] args){
 		Jugador a=new Jugador();
 		Jugador b=new Jugador();
-//		Jugador c=new Jugador();
-//		Jugador d=new Jugador();
-//		Jugador e=new Jugador();
-		Jugador [] arrjug=new Jugador [2];
+		Jugador c=new Jugador();
+		Jugador d=new Jugador();
+		Jugador e=new Jugador();
+		Jugador [] arrjug=new Jugador [5];
 		arrjug[0]=a;
 		arrjug[1]=b;
-//		arrjug[2]=c;
-//		arrjug[3]=d;
-//		arrjug[4]=e;
-		Propiedades prop =new Propiedades(7,8,7,2);
-		prop.repartirCartas(arrjug);
+		arrjug[2]=c;
+		arrjug[3]=d;
+		arrjug[4]=e;
+		Propiedades prop =new Propiedades(7,8,7,5);
+		prop.RepartirCartas(arrjug);
 		for(int i=0;i<arrjug.length;i++){
 			
 			for(int j=0;j<4;j++){
 				for(int h=0;h<arrjug[i].getMisCartas().get(j).size();h++){
-					System.out.println(arrjug[i].getMisCartas().get(j).get(h)+" "+j+" "+i);
+					System.out.println(arrjug[i].getMisCartas().get(j).get(h)+" "+h+" "+i);
 				}
 			}
 		}
